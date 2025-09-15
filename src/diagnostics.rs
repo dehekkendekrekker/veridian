94aa79650bd86d846482af151052e453be396b23
dhk <dhk@dehekkendekrekker.nl>
2025-09-15 15:45:07 -0500

    Removed slang extension

--- src/diagnostics.rs
+++ src/diagnostics.rs
@@ -1,70 +1,13 @@
 use crate::server::ProjectConfig;
-#[cfg(feature = "slang")]
-use path_clean::PathClean;
 use regex::Regex;
 use ropey::Rope;
-#[cfg(feature = "slang")]
-use std::env::current_dir;
-#[cfg(feature = "slang")]
-use std::path::Path;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
 use tower_lsp::lsp_types::*;
-#[cfg(feature = "slang")]
-use veridian_slang::slang_compile;
 use walkdir::DirEntry;
-#[cfg(feature = "slang")]
-use walkdir::WalkDir;
 use log::{debug, error};
 
-#[cfg(feature = "slang")]
-pub fn get_diagnostics(
-    uri: Url,
-    rope: &Rope,
-    files: Vec<Url>,
-    conf: &ProjectConfig,
-) -> PublishDiagnosticsParams {
-    if !(cfg!(test) && (uri.to_string().starts_with("file:///test"))) {
-        let paths = get_paths(files, conf.auto_search_workdir);
-        let mut diagnostics = {
-            if conf.verilator.syntax.enabled {
-                if let Ok(path) = uri.to_file_path() {
-                    verilator_syntax(
-                        rope,
-                        path,
-                        &conf.verilator.syntax.path,
-                        &conf.verilator.syntax.args,
-                    )
-                    .unwrap_or_default()
-                } else {
-                    Vec::new()
-                }
-            } else if conf.verible.syntax.enabled {
-                verible_syntax(rope, &conf.verible.syntax.path, &conf.verible.syntax.args)
-                    .unwrap_or_default()
-            } else {
-                Vec::new()
-            }
-        };
-        diagnostics.append(&mut parse_report(
-            uri.clone(),
-            slang_compile(paths).unwrap(),
-        ));
-        PublishDiagnosticsParams {
-            uri,
-            diagnostics,
-            version: None,
-        }
-    } else {
-        PublishDiagnosticsParams {
-            uri,
-            diagnostics: Vec::new(),
-            version: None,
-        }
-    }
-}
 
-#[cfg(not(feature = "slang"))]
 pub fn get_diagnostics(
     uri: Url,
     rope: &Rope,
@@ -113,102 +56,12 @@ pub fn get_diagnostics(
     }
 }
 
-/// recursively find source file paths from working directory
-/// and open files
-#[cfg(feature = "slang")]
-fn get_paths(files: Vec<Url>, search_workdir: bool) -> Vec<PathBuf> {
-    // check recursively from working dir for source files
-    let mut paths: Vec<PathBuf> = Vec::new();
-    if search_workdir {
-        let walker = WalkDir::new(".").into_iter();
-        for entry in walker.filter_entry(|e| !is_hidden(e)) {
-            let entry = entry.unwrap();
-            if entry.file_type().is_file() {
-                let extension = entry.path().extension().unwrap();
-
-                if extension == "sv" || extension == "svh" || extension == "v" || extension == "vh"
-                {
-                    paths.push(entry.path().to_path_buf());
-                }
-            }
-        }
-    }
-
-    // check recursively from opened files for source files
-    for file in files {
-        if let Ok(path) = file.to_file_path() {
-            if !paths.contains(&path) {
-                let walker = WalkDir::new(path.parent().unwrap()).into_iter();
-                for entry in walker.filter_entry(|e| !is_hidden(e)).flatten() {
-                    if entry.file_type().is_file() && entry.path().extension().is_some() {
-                        let extension = entry.path().extension().unwrap();
-
-                        if extension == "sv"
-                            || extension == "svh"
-                            || extension == "v"
-                            || extension == "vh"
-                        {
-                            let entry_path = entry.path().to_path_buf();
-                            if !paths.contains(&entry_path) {
-                                paths.push(entry_path);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    paths
-}
-
 pub fn is_hidden(entry: &DirEntry) -> bool {
     entry
         .file_name()
         .to_str()
         .map(|s| s.starts_with('.'))
         .unwrap_or(false)
-}
-
-#[cfg(feature = "slang")]
-/// parse a report from slang
-fn parse_report(uri: Url, report: String) -> Vec<Diagnostic> {
-    let mut diagnostics: Vec<Diagnostic> = Vec::new();
-    for line in report.lines() {
-        let diag: Vec<&str> = line.splitn(5, ':').collect();
-        if absolute_path(diag.first().unwrap()) == uri.to_file_path().unwrap().as_os_str() {
-            let pos = Position::new(
-                diag.get(1).unwrap().parse::<u32>().unwrap() - 1,
-                diag.get(2).unwrap().parse::<u32>().unwrap() - 1,
-            );
-            diagnostics.push(Diagnostic::new(
-                Range::new(pos, pos),
-                slang_severity(diag.get(3).unwrap()),
-                None,
-                Some("slang".to_owned()),
-                (*diag.get(4).unwrap()).to_owned(),
-                None,
-                None,
-            ))
-        }
-    }
-    diagnostics
-}
-
-#[cfg(feature = "slang")]
-fn slang_severity(severity: &str) -> Option<DiagnosticSeverity> {
-    match severity {
-        " error" => Some(DiagnosticSeverity::ERROR),
-        " warning" => Some(DiagnosticSeverity::WARNING),
-        " note" => Some(DiagnosticSeverity::INFORMATION),
-        _ => None,
-    }
-}
-
-#[cfg(feature = "slang")]
-// convert relative path to absolute
-fn absolute_path(path_str: &str) -> PathBuf {
-    let path = Path::new(path_str);
-    current_dir().unwrap().join(path).clean()
 }
 
 /// convert captured severity string to DiagnosticSeverity
@@ -373,35 +226,7 @@ mod tests {
     use tempdir::TempDir;
 
     #[test]
-    #[cfg(feature = "slang")]
-    fn test_diagnostics() {
-        test_init();
-        let uri = Url::from_file_path(absolute_path("test_data/diag/diag_test.sv")).unwrap();
-        let expected = PublishDiagnosticsParams::new(
-            uri.clone(),
-            vec![Diagnostic::new(
-                Range::new(Position::new(3, 13), Position::new(3, 13)),
-                Some(DiagnosticSeverity::WARNING),
-                None,
-                Some("slang".to_owned()),
-                " cannot refer to element 2 of \'logic[1:0]\' [-Windex-oob]".to_owned(),
-                None,
-                None,
-            )],
-            None,
-        );
-        let diag = get_diagnostics(
-            uri.clone(),
-            &Rope::default(),
-            vec![uri],
-            &ProjectConfig::default(),
-        );
-        assert_eq!(diag.uri, expected.uri);
-        assert_eq!(diag.version, expected.version);
-        assert_eq!(diag.diagnostics.last(), expected.diagnostics.last());
-    }
-
-    #[test]
+   #[test]
     fn test_unsaved_file() {
         test_init();
         let uri = Url::parse("file://test.sv").unwrap();
